f1e1b001d52599f4f007e08a67b4829095704178
Rogério Prando <rogprando@gmail.com>
2022-10-21 14:11:56 -0300

    props, dispatch e bind

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
     import Titulo from "./components/Titulo.svelte";
-    import BarraSuperior from "./components/BarraSuperior.svelte";
     import type IUsuario from "./interfaces/IUsuario";
+    import Usuario from "./components/Usuario.svelte";
+    import Formulario from "./components/Formulario.svelte";
 
-    let usuario: IUsuario = null;
+    // se "strictNullChecks": true temos que dizer pro compilador TS que usuarioApp pode ser nulo ou IUsuario
+    let usuarioApp: IUsuario | null = null;
 
-    let valorInput;
-
-    function aoSubmeter() {
-        usuario = {
-            avatar_url: "https://github.com/rogerioprando.png",
-            login: "rogerioprando",
-            nome: "Rogério Prando",
-            perfil_url: "https://github.com/rogerioprando",
-            repositorios_publicos: 23,
-            seguidores: 13,
-        };
+    function definirUsuario(evento: CustomEvent<IUsuario>){
+        // Variável pai recebe os eventos que a variável filho ta enviando (Via dispatch)
+        usuarioApp = evento.detail;
     }
 </script>
 
@@ -24,51 +18,27 @@
         <Titulo />
 
         <div class="busca-usuario">
-            <form on:submit|preventDefault={aoSubmeter}>
-                <input type="text" class="input" bind:value={valorInput} />
-
-                <div class="botao-container">
-                    <button type="submit" class="botao">Buscar</button>
-                </div>
-            </form>
+            <!--
+                USANDO BIND:
+                Aqui precisamos fazer bind pois queremos que a prop Filho (Formulario) passe pro Pai (App)
+                Se as variáveis dos componentes possuisseem mesmo nome (usuario) basta deixar assim
+                <Formulario bind:usuario />
+                <Formulario bind:usuarioForm={usuarioApp} />
+            -->
+            <!-- 
+                USANDO DISPATCH
+            -->
+        <Formulario on:aoAlterarUsuario={definirUsuario}/>
         </div>
     </header>
 
-    {#if usuario}
-    <div class="card-usuario">
-        <BarraSuperior />
-
-        <div class="usuario">
-            <div class="foto-container">
-                <!-- svelte-ignore security-anchor-rel-noreferrer -->
-                <a
-                    href="https://github.com/rogerioprando"
-                    target="_blank"
-                    rel="noopener"
-                >
-                    <div
-                        class="foto-usuario"
-                        style:background-image="url({usuario.avatar_url})"
-                    />
-                </a>
-            </div>
-
-            <div class="detalhes-usuario">
-                <div class="info">
-                    Nome: <span>{usuario.nome}</span>
-                </div>
-                <div class="info">
-                    Usuário: <span>{usuario.login}</span>
-                </div>
-                <div class="info">
-                    Seguidores: <span>{usuario.seguidores}</span>
-                </div>
-                <div class="info">
-                    Repositórios: <span>{usuario.repositorios_publicos}</span>
-                </div>
-            </div>
-        </div>
-    </div>
+    {#if usuarioApp}
+        <!--
+            Se o nome da váriavel usuario fosse igual a propriedade usuario do componente bastaria passar
+            <Usuario {usuario} />
+            Aqui está sendo passado uma prop de Pai (App) para Filho (Usuario)
+        -->
+        <Usuario usuario={usuarioApp} />
     {/if}
 </div>
 
@@ -86,96 +56,5 @@
     .busca-usuario {
         position: relative;
         width: 70%;
-    }
-
-    .input {
-        padding: 15px 25px;
-        width: calc(100% - 8.75rem);
-        font-size: 1rem;
-        border-radius: 8px;
-        border: 1px solid #2e80fa;
-        box-shadow: 0px 17px 52px rgba(222, 231, 247, 0.4);
-        outline: 0;
-    }
-
-    .input::placeholder {
-        font-family: "Roboto";
-        font-style: italic;
-        font-weight: 300;
-        font-size: 19.5px;
-        line-height: 26px;
-        color: #6e8cba;
-    }
-
-    .botao-container {
-        position: absolute;
-        width: 9.625rem;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        display: flex;
-    }
-
-    .botao {
-        padding: 15px 24px;
-        border-radius: 8px;
-        border: none;
-        background: #2e80fa;
-        line-height: 26px;
-        color: #fff;
-        font-size: 22px;
-        cursor: pointer;
-
-        transition: background-color 0.2s;
-
-        display: flex;
-        align-items: center;
-        gap: 13px;
-    }
-
-    .botao:hover {
-        background: #4590ff;
-    }
-
-    .card-usuario {
-        margin-top: 65px;
-    }
-
-    .usuario {
-        padding: 28px 0;
-        background: rgba(255, 255, 255, 0.5);
-        box-shadow: -12px 37px 45px rgba(133, 127, 201, 0.18);
-        border-radius: 0px 0px 13px 13px;
-
-        display: flex;
-        justify-content: center;
-    }
-
-    .foto-container {
-        margin-right: 81px;
-    }
-
-    .foto-usuario {
-        width: 12.75rem;
-        height: 12.75rem;
-        border: 4.56px solid #2e80fa;
-        border-radius: 50%;
-        background-size: cover;
-    }
-
-    .detalhes-usuario {
-        margin-right: 55px;
-    }
-
-    .detalhes-usuario > .info {
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 31px;
-        color: #395278;
-    }
-
-    .detalhes-usuario > .info > span {
-        color: #6781a8;
-        font-weight: normal;
     }
 </style>
---
+++ src/components/Formulario.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+    import type IUsuario from "src/interfaces/IUsuario";
+    import { createEventDispatcher } from "svelte";
+
+    let valorInput: string;
+
+    // Só precisa se usar  bind
+    // export let usuarioForm: IUsuario | null;
+
+    // dispatch vai fazer com que a var filha envie o conteúdo pra var pai
+    const dispatch = createEventDispatcher<{
+        aoAlterarUsuario: IUsuario
+    }>();
+
+    async function aoSubmeter() {
+        // retorno do fetch é uma Promise (portanto a função precisa ser async)
+        const respostaUsuario = await fetch(
+            `https://api.github.com/users/${valorInput}`
+        );
+        const dadosUsuario = await respostaUsuario.json();
+
+        //console.log(dadosUsuario);
+
+        // Função dispatch dispara o evento aoAlterarUsuario com as infos do usuario.
+        // Alguém precisa escutar esse evento.
+        dispatch('aoAlterarUsuario', {
+            avatar_url: dadosUsuario.avatar_url,
+            login: dadosUsuario.login,
+            nome: dadosUsuario.name,
+            perfil_url: dadosUsuario.html_url,
+            repositorios_publicos: dadosUsuario.public_repos,
+            seguidores: dadosUsuario.followers,
+        });
+
+        // Usando bind precisa deixar essa parte:
+        // usuarioForm = {
+        //     avatar_url: dadosUsuario.avatar_url,
+        //     login: dadosUsuario.login,
+        //     nome: dadosUsuario.name,
+        //     perfil_url: dadosUsuario.html_url,
+        //     repositorios_publicos: dadosUsuario.public_repos,
+        //     seguidores: dadosUsuario.followers,
+        // };
+    }
+</script>
+
+<form on:submit|preventDefault={aoSubmeter}>
+    <input
+        type="text"
+        class="input"
+        bind:value={valorInput}
+        placeholder="Pesquise o usuário"
+    />
+
+    <div class="botao-container">
+        <button type="submit" class="botao">Buscar</button>
+    </div>
+</form>
+
+<style>
+    .input {
+        padding: 15px 25px;
+        width: calc(100% - 8.75rem);
+        font-size: 1rem;
+        border-radius: 8px;
+        border: 1px solid #2e80fa;
+        box-shadow: 0px 17px 52px rgba(222, 231, 247, 0.4);
+        outline: 0;
+    }
+
+    .input::placeholder {
+        font-family: "Roboto";
+        font-style: italic;
+        font-weight: 300;
+        font-size: 19.5px;
+        line-height: 26px;
+        color: #6e8cba;
+    }
+
+    .botao-container {
+        position: absolute;
+        width: 9.625rem;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        display: flex;
+    }
+
+    .botao {
+        padding: 15px 24px;
+        border-radius: 8px;
+        border: none;
+        background: #2e80fa;
+        line-height: 26px;
+        color: #fff;
+        font-size: 22px;
+        cursor: pointer;
+
+        transition: background-color 0.2s;
+
+        display: flex;
+        align-items: center;
+        gap: 13px;
+    }
+
+    .botao:hover {
+        background: #4590ff;
+    }
+</style>
